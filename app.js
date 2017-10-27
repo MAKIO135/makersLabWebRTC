1cda5367e272e93faa10c8cba343135576babe66
damien.chevalier <damien.chevalier@epitech.eu>
2017-10-27 10:44:12 +0200

    changing to es5

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ var screensharingusers = {};
 var socketio;
 var trimmed;
 server.listen(process.env.PORT || 5000);
-console.log(`App started at port ${process.env.PORT||5000}`);
+console.log('App started at port' + (process.env.PORT||5000) );
 app.use(express.static(__dirname + '/public'));
 app.get('/',function(req,res){
 	res.sendfile(__dirname+'/public/404.html');
